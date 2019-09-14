044a5a0cad95a03d92980bba77c0c04cee35e6bc
Erick <henrique.eryck@gmail.com>
2019-09-14 12:19:53 -0300

    .

--- Exercise/List II/Ex_02.cpp
+++ Exercise/List II/Ex_02.cpp
@@ -10,7 +10,7 @@ int main(int argc, char** argv)
 	cin >> cl;
 	
 	if (cl >= 50){
-		cout << "Seu gasto esta alto!"<< endl;
+		cout << "Seu gasto esta alto !"<< endl;
 	} 
 	else{
 		cout <<"Gasto na media " << cl << endl;
