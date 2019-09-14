3e3157c038b59f33a5adb9072ec23ad7a57e5dd5
Erick <henrique.eryck@gmail.com>
2019-09-14 11:24:07 -0300

    .

---
+++ Exercise/List I/Ex_10.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	float A,B;
+	
+	cout <<"Digite o valor para A : " << endl; 
+	cin >> A;
+	
+	cout <<"Digite o valor para B" << endl; 
+	cin >> B;
+	
+	
+	cout <<"A vale >>" << B << endl;
+	cout <<"B vale >>" << A << endl;
+	
+	
+	return 0;
+}
