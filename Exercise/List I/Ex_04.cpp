52bcb126e9f20a84c1b806ad902c2909bff208ca
Erick <henrique.eryck@gmail.com>
2019-09-14 10:30:04 -0300

    .

--- Exercise/List I/Ex_04.cpp
+++ Exercise/List I/Ex_04.cpp
@@ -7,7 +7,7 @@ int main(int argc, char** argv)
 	float pr, d, pag, novo;
 	
 	cout <<"Digite o valor do produto" << endl; 
-	cin >> preco;
+	cin >> pr;
 	
 	cout <<"Digite o desconto do produto" << endl; 
 	cin >> d;
@@ -16,8 +16,8 @@ int main(int argc, char** argv)
 	pag= (pr * d /100);
 	novo = pr - (pr * d /100);
 	
-	cout <<"O preco do produto é  " << preco << " com 5% desconto == " << novo << endl;
-	cout << "Com desconto de " << d << "%" << endl;
+	cout <<"O preco do produto e :  " << pr << endl;
+	cout << "Desconto de  " << d << "%" << endl;
 	cout << "Total : " << novo <<endl;
 	
 	return 0;
